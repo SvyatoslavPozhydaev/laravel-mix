c816aa1d553f38f127c7132ac96e83011857ce28
SvyatoslavPozhydaev <svyatreal@gmail.com>
2018-10-27 08:46:24 +0300

    update dependency, add otional img-loader for images

--- src/builder/webpack-rules.js
+++ src/builder/webpack-rules.js
@@ -8,43 +8,50 @@ module.exports = function() {
     });
 
     // Add support for loading images.
-    rules.push({
-        // only include svg that doesn't have font in the path or file name by using negative lookahead
-        test: /(\.(png|jpe?g|gif)$|^((?!font).)*\.svg$)/,
-        loaders: [
-            {
-                loader: 'file-loader',
-                options: {
-                    name: path => {
-                        if (!/node_modules|bower_components/.test(path)) {
-                            return (
-                                Config.fileLoaderDirs.images +
-                                '/[name].[ext]?[hash]'
-                            );
-                        }
+    let imageRule = {
+      // only include svg that doesn't have font in the path or file name by using negative lookahead
+      test: /(\.(png|jpe?g|gif)$|^((?!font).)*\.svg$)/,
+      loaders: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: path => {
+              if (!/node_modules|bower_components/.test(path)) {
+                return (
+                  Config.fileLoaderDirs.images +
+                  '/[name].[ext]?[hash]'
+                );
+              }
 
-                        return (
-                            Config.fileLoaderDirs.images +
-                            '/vendor/' +
-                            path
-                                .replace(/\\/g, '/')
-                                .replace(
-                                    /((.*(node_modules|bower_components))|images|image|img|assets)\//g,
-                                    ''
-                                ) +
-                            '?[hash]'
-                        );
-                    },
-                    publicPath: Config.resourceRoot
-                }
+              return (
+                Config.fileLoaderDirs.images +
+                '/vendor/' +
+                path
+                .replace(/\\/g, '/')
+                .replace(
+                  /((.*(node_modules|bower_components))|images|image|img|assets)\//g,
+                  ''
+                ) +
+                '?[hash]'
+              );
             },
+            publicPath: Config.resourceRoot
+          }
+        }
+      ]
+    }
 
-            {
-                loader: 'img-loader',
-                options: Config.imgLoaderOptions
-            }
-        ]
-    });
+    try {
+      let imgLoader = require('img-loader')
+      imageRule.loaders.push({
+        loader: 'img-loader',
+        options: Config.imgLoaderOptions
+      })
+    } catch (e) {
+      console.warn('without img-loader')
+    }
+
+    rules.push(imageRule);
 
     // Add support for loading fonts.
     rules.push({
